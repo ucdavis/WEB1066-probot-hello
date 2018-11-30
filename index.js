7f949f02c94537c136d2e50144a0b7b3e3f4ea05
Edward Raigosa <wenlock@github.com>
2018-11-29 22:27:58 -0800

    comment out more

--- index.js
+++ index.js
@@ -2,29 +2,29 @@
  * This is the entry point for your Probot App.
  * @param {import('probot').Application} app - Probot's Application class.
  */
- function objToString(arr,level) {
-     var dumped_text = "";
-     if(!level) level = 0;
-
-     var level_padding = "";
-     for(var j=0;j<level+1;j++) level_padding += "    ";
-
-     if(typeof(arr) == 'object') {
-         for(var item in arr) {
-             var value = arr[item];
-
-             if(typeof(value) == 'object') {
-                 dumped_text += level_padding + "'" + item + "' ...\n";
-             //     dumped_text += objToString(value,level+1);
-             } else {
-                 dumped_text += level_padding + "'" + item + "' => \"" + value + "\"\n";
-             }
-         }
-     } else {
-         dumped_text = "===>"+arr+"<===("+typeof(arr)+")";
-     }
-     return dumped_text;
-}
+//  function objToString(arr,level) {
+//      var dumped_text = "";
+//      if(!level) level = 0;
+//
+//      var level_padding = "";
+//      for(var j=0;j<level+1;j++) level_padding += "    ";
+//
+//      if(typeof(arr) == 'object') {
+//          for(var item in arr) {
+//              var value = arr[item];
+//
+//              if(typeof(value) == 'object') {
+//                  dumped_text += level_padding + "'" + item + "' ...\n";
+//              //     dumped_text += objToString(value,level+1);
+//              } else {
+//                  dumped_text += level_padding + "'" + item + "' => \"" + value + "\"\n";
+//              }
+//          }
+//      } else {
+//          dumped_text = "===>"+arr+"<===("+typeof(arr)+")";
+//      }
+//      return dumped_text;
+// }
 
 module.exports = app => {
   // Get an express router to expose new HTTP endpoints
