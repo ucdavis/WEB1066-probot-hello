431a287d75710a1eaeb6f8632b2568558a564040
hajlaoui <hajlaouijalel.ig@gmail.com>
2020-06-07 04:45:25 -0700

    Change vaulue
    11 -> 1

--- test/index.test.js
+++ test/index.test.js
@@ -6,7 +6,7 @@ const issuesOpenedPayload = require('./fixtures/issues.opened.json')
 
 test('that we can run tests', () => {
   // your real tests go here
-  expect(11 + 2 + 3).toBe(6)
+  expect(1 + 2 + 3).toBe(6)
 }) 
 //The describe instruction
 describe('My Probot app', () => {
