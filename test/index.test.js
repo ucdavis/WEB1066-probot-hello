14d0c0162a4bc3dca226339cce0c96ed53b51c4c
hajlaoui <hajlaouijalel.ig@gmail.com>
2020-06-07 03:05:30 -0700

    change value 1
    We have changer the value 1 by 11

--- test/index.test.js
+++ test/index.test.js
@@ -6,7 +6,7 @@ const issuesOpenedPayload = require('./fixtures/issues.opened.json')
 
 test('that we can run tests', () => {
   // your real tests go here
-  expect(1 + 2 + 3).toBe(6)
+  expect(11 + 2 + 3).toBe(6)
 }) 
 
 describe('My Probot app', () => {
